18aa238327e91a807472694343981c4608e9b97c
roninio <azachir@gmail.com>
2014-04-23 15:24:17 +0300

    sorting objects

---
+++ src/com/example/helloword/HelloWorldTest.java
@@ -0,0 +1,64 @@
+package com.example.helloword;
+
+import java.util.Collections;
+
+/**
+ * Created by ronen on 4/22/14.
+ */
+public class HelloWorldTest extends junit.framework.TestCase {
+
+    Color RED = new Color("RED", 0);
+    Color GREEN =new Color("GREEN", 1);
+    Color BLUE = new Color("BLUE", 2);
+
+    public void testsortObjects() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        Bicycle[] copyFrom = {new Bicycle(RED),new Bicycle(GREEN),new Bicycle(RED)};
+
+        Bicycle[] newArray = helloWorld.sortObjects(copyFrom);
+
+        checkArray(new String[]{"RED","RED","GREEN"}, newArray);
+
+    }
+
+    public void testSortObjects2() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        Bicycle[] copyFrom = {new Bicycle(RED),new Bicycle(GREEN),new Bicycle(BLUE),new Bicycle(RED),new Bicycle(RED),new Bicycle(RED)};
+
+        Bicycle[] newArray = helloWorld.sortObjects(copyFrom);
+
+        checkArray(new String[]{"RED","RED","RED","RED","GREEN","BLUE"}, newArray);
+
+    }
+
+    public void testSortObjects3() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        Bicycle[] copyFrom = {new Bicycle(BLUE),new Bicycle(GREEN),new Bicycle(BLUE),new Bicycle(RED),new Bicycle(GREEN),new Bicycle(GREEN)};
+
+        Bicycle[] newArray = helloWorld.sortObjects(copyFrom);
+
+        checkArray(new String[]{"RED","GREEN","GREEN","GREEN","BLUE","BLUE"}, newArray);
+
+    }
+
+    // introducing a new color
+    public void testSortObjects5() throws Exception {
+
+        Color KKKK = new Color("KKKK", 3);
+        HelloWorld helloWorld = new HelloWorld(4);
+        Bicycle[] copyFrom = {new Bicycle(BLUE),new Bicycle(GREEN),new Bicycle(BLUE),new Bicycle(RED),new Bicycle(GREEN),new Bicycle(KKKK)};
+
+        Bicycle[] newArray = helloWorld.sortObjects(copyFrom);
+
+        checkArray(new String[]{"RED","GREEN","GREEN","BLUE","BLUE","KKKK"}, newArray);
+
+    }
+
+    private void checkArray(String[] colors, Bicycle[] list){
+
+        for(int i=0;i< colors.length;i++){
+            assertEquals("validate color index = " + i, colors[i], list[i].getColor().getColor()) ;
+
+        }
+    }
+}
